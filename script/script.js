00e986ea4fc5e8b1871361d9fe2b17f2a26c805f
Konrad <olchakonrad@gmail.com>
2019-12-02 22:40:59 +0100

    strona technologie i scrollowanie

---
+++ script/script.js
@@ -0,0 +1,45 @@
+const $hamburgerBtn = $('.header__hamburger');
+const $menu = $('.header__navigation');
+const $btnGoTop = $('.btnGoTop').hide()
+if($(window).width() > 550) {
+    $menu.show()
+} else {
+    $menu.hide()
+}
+$hamburgerBtn.on("click", ()=> {
+    $menu.slideToggle()
+})
+$(window).on("scroll", (e) => {
+    if($(window).scrollTop() > 500) {
+        $btnGoTop.show()
+    } else {
+        $btnGoTop.hide()
+    }
+})
+$btnGoTop.on("click", () => {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 'slow')
+})
+
+$(window).on("resize",function(){  
+    if($(window).width() > 550) {
+        $menu.show()
+    } else {
+        $menu.hide()
+    }
+ })
+ function scrollTo (element) {
+        $('html, body').animate({
+            scrollTop: ($(element).offset().top) - 60
+        }, 'slow')
+ }
+ $('.content__li--frontend').on('click', () => {
+    scrollTo('.content__title--frontend')
+ })
+ $('.content__li--backend').on('click', () => {
+    scrollTo('.content__title--backend')
+ })
+ $('.content__li--db').on('click', () => {
+    scrollTo('.content__title--db')
+ })
